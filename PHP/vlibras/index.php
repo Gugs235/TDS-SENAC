d15cbb0d8df9b8992be37df1f9c5731a8287c7f9
Gugs235 <jhonathan.ssoares2008@gmail.com>
2025-09-16 14:07:07 -0400

    vlibras
    aprendemos a usar o vlibras para implantar no PI

---
+++ PHP/vlibras/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+
+    <h1>Página de Libras</h1>
+
+</body>
+
+</html>
